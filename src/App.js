2a78260563cec9a4c07ba707c15cb09a3988d480
sumeyyesever <sumeysever@gmail.com>
2023-01-12 21:42:50 +0300

    complete post folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import TopBar from "./topbar/TopBar";
+import TopBar from "./components/topbar/TopBar"
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-  <TopBar />
+    <>
+ <TopBar />
+  <Home />
+    </>
+ 
   );
 }
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Posts from "../../components/posts/Posts";
+import "./home.css";
+
+export default function Home(){
+    return (
+        <div className="home">
+<Posts />
+        </div>
+        
+    )
+}
